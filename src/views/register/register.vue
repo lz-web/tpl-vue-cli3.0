83af6b16a9ceed0b864325b7d386b243a17ebe71
hypering <hypering@hyperingdeMacBook-Pro.local>
2020-04-26 16:21:54 +0800

    注册 登录 基本完成

--- src/views/register/register.vue
+++ src/views/register/register.vue
@@ -7,92 +7,84 @@
 
                 </div>
                 <!-- 注册内容框 -->
-                <div class="register-inner">
+                <div class="register-inner flex-col-between">
+                    <!-- 注册标题 -->
                     <div class="register-title">
-                        <span>注册</span>
-                        <router-link class="to-login" to="/login">已有账号，去登录></router-link>
-                        <!-- <div>
-                            <span>已有账号，去登录</span>
-                            <el-image
-                                style="width: 100px; height: 100px"
-                                :src="url"
-                                :fit="fit"></el-image>
-                        </div> -->
+                        <span class="register-title-cont">注册</span>
+                        <router-link class="to-login flex-center" to="/login">已有账号，去登录></router-link>
                     </div>
-                    <!-- 第一步 微信扫码绑定 -->
-                    <div v-show="step_stu == 0" class="main-wrap">
-                        <div class="main-item">
-                            <span class="qrcode-title">支持微信扫码登录</span>
-                            <el-image
-                                class="qrcode"
-                                :src="qrcode"
-                                ></el-image>
-                            <div class="btn-wrap">
-                                <el-button class="ts-input" @click="nextBtn()" type="primary">下一步</el-button>
+
+                    <!-- 注册步骤 -->
+                    <div class="register-info">
+                        <!-- 第一步 微信扫码绑定 -->
+                        <div v-show="step_stu == 0" class="main-wrap">
+                            <div class="qrcode-bind flex-col-center">
+                                <span class="qrcode-title">支持微信扫码登录</span>
+                                <div class="qrcode-inner flex-center">
+                                    <el-image
+                                    class="qrcode-cont"
+                                    :src="qrcode"
+                                    ></el-image>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <!-- 第二步 基础信息 -->
-                    <div v-show="step_stu == 1" class="main-wrap">
-                        <div class="main-item">
-                            <el-input class="ts-input" v-model="user_phone" placeholder="请输入手机号"></el-input>
-                        </div>
-                        <div class="main-item">
-                            <el-input class="ts-input" v-model="user_pwd" placeholder="请输入至少6位密码，不能包含特殊字符"></el-input>
-                        </div>
-                        <div class="main-item">
-                            <el-input class="ts-input" v-model="user_pwd2" placeholder="请再次输入密码"></el-input>
-                        </div>
-                        <div class="main-item">
-                            <el-select class="ts-input" v-model="user_question_select" placeholder="请选择密保问题">
-                                <el-option
+                        <!-- 第二步 基础信息 -->
+                        <div v-show="step_stu == 1" class="main-wrap">
+                            <div class="main-item">
+                                <el-input class="ts-input" v-model="user_phone" placeholder="请输入手机号"></el-input>
+                            </div>
+                            <div class="main-item">
+                                <el-input class="ts-input" v-model="user_pwd" placeholder="请输入至少6位密码，不能包含特殊字符"></el-input>
+                            </div>
+                            <div class="main-item">
+                                <el-input class="ts-input" v-model="user_pwd2" placeholder="请再次输入密码"></el-input>
+                            </div>
+                            <div class="main-item">
+                                <el-select class="ts-input" v-model="user_question_select" placeholder="请选择密保问题">
+                                    <el-option
                                     v-for="item in user_question_list"
                                     :key="item.value"
                                     :label="item.label"
                                     :value="item.value">
-                                </el-option>
-                            </el-select>
-                        </div>
-                        <div class="main-item">
-                            <el-date-picker
+                                    </el-option>
+                                </el-select>
+                            </div>
+                            <div class="main-item">
+                                <el-date-picker
                                 class="ts-input"
                                 v-model="user_question_ret"
                                 type="date"
                                 placeholder="选择日期"
                                 format="yyyy 年 MM 月 dd 日"
                                 ></el-date-picker>
+                            </div>
                         </div>
-                        <div class="btn-wrap">
-                            <el-button class="ts-input" @click="nextBtn()" type="primary">下一步</el-button>
-                        </div>
-                    </div>
-                    <!-- 第三步 详细信息 -->
-                    <div v-show="step_stu == 2" class="main-wrap">
-                        <div class="main-item">
-                            <el-input class="ts-input" v-model="user_name" placeholder="请输入姓名"></el-input>
-                        </div>
-                        <div class="main-item">
-                            <el-input class="ts-input" v-model="user_industry" placeholder="行业类型"></el-input>
-                        </div>
-                        <div class="main-item">
-                            <el-input class="ts-input" v-model="user_company" placeholder="公司/单位名称"></el-input>
-                        </div>
-                        <div class="main-item">
-                            <el-input class="ts-input" v-model="user_class" placeholder="请输入职位"></el-input>
-                        </div>
-                        <div class="main-item">
-                            <el-input class="ts-input" v-model="company_phone" placeholder="请输入公司电话?"></el-input>
-                        </div>
-                        <!-- <div class="main-item">
-                            <el-input type="textarea" :rows="2" placeholder="请输入申请理由" v-model="applay_reason"></el-input>
-                        </div> -->
-                        <div class="btn-wrap">
-                            <el-button class="ts-input" @click="submitBtn()" type="primary">提交</el-button>
+                        <!-- 第三步 详细信息 -->
+                        <div v-show="step_stu == 2" class="main-wrap">
+                            <div class="main-item">
+                                <el-input class="ts-input" v-model="user_name" placeholder="请输入姓名"></el-input>
+                            </div>
+                            <div class="main-item">
+                                <el-input class="ts-input" v-model="user_industry" placeholder="行业类型"></el-input>
+                            </div>
+                            <div class="main-item">
+                                <el-input class="ts-input" v-model="user_company" placeholder="公司/单位名称"></el-input>
+                            </div>
+                            <div class="main-item">
+                                <el-input class="ts-input" v-model="user_class" placeholder="请输入职位"></el-input>
+                            </div>
+                            <div class="main-item">
+                                <el-input class="ts-input" v-model="company_phone" placeholder="请输入公司电话?"></el-input>
+                            </div>
                         </div>
                     </div>
-                    <!-- 协议 -->
-                    <div class="register-checkbox">
-                        <el-checkbox v-model="user_checked">同意《用户隐私协议》</el-checkbox>
+
+                    <!-- 注册底部按钮 -->
+                    <div class="register-button">
+                        <!-- 提交按钮 -->
+                        <el-button class="ts-input" @click="verifyStep()" type="primary">{{step_stu == 0 || step_stu == 1 ? '下一步' : '提交'}}</el-button>
+                        <!-- 协议 -->
+                        <el-checkbox class="register-checkbox" v-model="user_checked">同意《用户隐私协议》</el-checkbox>
                     </div>
                 </div>
             </div>
@@ -103,7 +95,6 @@
 <script lang="ts" src="./register.ts"></script>
 
 <style lang="scss">
-
-    @import "./register.scss";
+@import "./register.scss";
 </style>
 
