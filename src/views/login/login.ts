adc922874bc7ce1bd35898d2edb97606122c7046
HuiYoujie <42954611+HuiYoujie@users.noreply.github.com>
2020-04-27 23:41:25 +0800

    注册(勾选协议未完成) 登录(忘记密码未完成)

--- src/views/login/login.ts
+++ src/views/login/login.ts
@@ -14,13 +14,19 @@ export default class LoginVue extends Vue {
     // @Action GET_DATA_ASYN
 
     // Variablet Wrap   eg : private user_name : string = 'root';
-    user_name: string = '15751668553'; // 用户名
+    user_phone: string = '15751668553'; // 用户名
     user_pwd: string = 'hui123456'; // 密码
     user_verify_code: string | number = '1234'; // 验证码
     remember_pwd: boolean = false;
     account: boolean = true;
     active_name: any = 'first'; // 当前点击tab
     qrcode: string = '';
+
+    show_set_pwd: boolean = false;
+    user_question_ret: any = '';
+    forget: boolean = true;
+    user_pwd_set: string = '';
+    user_pwd_set2: string = '';
     created() {
         //
         console.log(this.$echarts)
@@ -50,7 +56,7 @@ export default class LoginVue extends Vue {
     // 用户登录按钮
     loginSubmit() {
         Api.postLogin({
-            user_phone: this.user_name,
+            user_phone: this.user_phone,
             user_pwd: this.user_pwd
         }).then((res: any) => {
             console.log(res)
@@ -70,6 +76,71 @@ export default class LoginVue extends Vue {
                 this.$message({
                     message: res.msg,
                     type: 'error'
+                })
+            }
+        })
+    }
+
+    // 忘记密码
+    forgetPwd() {
+        this.show_set_pwd = true
+    }
+
+    // 取消(忘记密码)
+    cancel() {
+        this.show_set_pwd = false
+        this.forget = true
+    }
+
+    // 提交(忘记密码)
+    commit() {
+        if (!this.user_question_ret) {
+            this.$message({
+                type: 'error',
+                message: '密保问题答案不能为空!'
+            })
+            // return
+        }
+        // 验证密保答案
+
+        this.forget = false
+    }
+
+    // 确定(重置密码)
+    confirm() {
+        if (!this.user_pwd_set) {
+            this.$message({
+                type: 'error',
+                message: '新密码不能为空!'
+            })
+            return
+        }
+        if (!this.user_pwd_set2) {
+            this.$message({
+                type: 'error',
+                message: '确认密码不能为空!'
+            })
+            return
+        }
+        if (this.user_pwd_set != this.user_pwd_set2) {
+            this.$message({
+                type: 'error',
+                message: '两次密码不一致!'
+            })
+            return
+        }
+        Api.changePassword({
+            user_phone: this.user_phone,
+            user_pwd_set: this.user_pwd_set
+        }).then((res: any) => {
+            console.log(res)
+            if (res.code == 200) {
+                this.show_set_pwd = false
+                this.forget = true
+            } else {
+                this.$message({
+                    type: 'error',
+                    message: res.msg
                 })
             }
         })
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -2,46 +2,48 @@
     <div class="login-bg">
         <div class="hui-container flex-col-end">
             <div class="login-wrap">
-                <div class="login-inner flex-col-between">
+                <!-- 登录主体 -->
+                <div v-show="!show_set_pwd" class="login-inner flex-col-between">
                     <div class="login-cont">
+                        <!-- 登录标题 -->
                         <div class="login-title">{{account ? '账号登录' : '微信登录'}}</div>
-                            <!-- 账号登录 -->
-                            <div v-show="account" class="account-login">
-                                <div class="main-item">
-                                    <el-input
-                                    class="ts-input"
-                                    autocomplete="new-password"
-                                    v-model="user_name"
-                                    placeholder="请输入用户名"
-                                    ></el-input>
-                                </div>
-                                <div class="main-item">
-                                    <el-input
-                                    class="ts-input"
-                                    autocomplete="new-password"
-                                    placeholder="请输入密码"
-                                    v-model="user_pwd"
-                                    show-password
-                                    ></el-input>
-                                </div>
-                                <div class="main-item">
-                                    <el-input
-                                    class="ts-input"
-                                    autocomplete="new-password"
-                                    v-model="user_verify_code"
-                                    placeholder="请输入验证码"
-                                    ></el-input>
-                                </div>
-                                <div class="row-pwd flex-between">
-                                    <el-radio class="remember-pwd" v-model="remember_pwd" label>记住密码</el-radio>
-                                    <span class="forget-pwd">忘记密码</span>
+                        <!-- 账号登录 -->
+                        <div v-show="account" class="account-login">
+                            <div class="main-item">
+                                <el-input
+                                class="ts-input"
+                                autocomplete="new-password"
+                                v-model="user_phone"
+                                placeholder="请输入用户名"
+                                ></el-input>
                             </div>
-                                <div class="main-item">
-                                    <el-button class="ts-input" @click="loginSubmit" type="primary">登录</el-button>
-                                </div>
+                            <div class="main-item">
+                                <el-input
+                                class="ts-input"
+                                autocomplete="new-password"
+                                placeholder="请输入密码"
+                                v-model="user_pwd"
+                                show-password
+                                ></el-input>
                             </div>
-                            <!-- 微信扫码登录 -->
-                            <div v-show="!account" class="flex-col-center wx-login">
+                            <div class="main-item">
+                                <el-input
+                                class="ts-input"
+                                autocomplete="new-password"
+                                v-model="user_verify_code"
+                                placeholder="请输入验证码"
+                                ></el-input>
+                            </div>
+                            <div class="row-pwd flex-between">
+                                <el-radio class="remember-pwd" v-model="remember_pwd" label>记住密码</el-radio>
+                                <span class="forget-pwd" @click="forgetPwd">忘记密码</span>
+                            </div>
+                            <div class="main-item">
+                                <el-button class="ts-input" @click="loginSubmit" type="primary">登录</el-button>
+                            </div>
+                        </div>
+                        <!-- 微信扫码登录 -->
+                        <div v-show="!account" class="flex-col-center wx-login">
                             <span class="wx-login-title">支持微信扫码登录</span>
                             <div class="wx-login-inner flex-center">
                                 <el-image class="wx-login-cont" :src="qrcode"></el-image>
@@ -51,6 +53,43 @@
                     <div class="login-buttons flex-around">
                         <div class="login-way" @click="changeLoginWay">{{account ? '微信登录' : '账号登录'}}</div>
                         <router-link active-class to="/register">注册</router-link>
+                    </div>
+                </div>
+                
+                <!-- 忘记密码 重置密码 -->
+                <div v-show="show_set_pwd" class="pwd-inner flex-col-between">
+                    <div v-show="forget" class="forget-pwd-cont flex-col-between">
+                        <div class="flex-col">
+                            <span class="forget-pwd-title">忘记密码</span>
+                            <el-input
+                                class="ts-input"
+                                autocomplete="new-password"
+                                v-model="user_phone"
+                                placeholder="请输入用户名"
+                                ></el-input>
+                            <span>密保问题：{{}}</span>
+                            <el-date-picker
+                                class="ts-input"
+                                v-model="user_question_ret"
+                                type="date"
+                                placeholder="选择日期"
+                                format="yyyy 年 MM 月 dd 日"
+                                ></el-date-picker>
+                        </div>
+                        <div class="flex-start">
+                            <el-button class="ts-input" @click="cancel()" type="primary">取消</el-button>
+                            <el-button class="ts-input" @click="commit()" type="primary">提交</el-button>
+                        </div>
+                    </div>
+                    <div v-show="!forget" class="set-pwd-cont flex-col-between">
+                        <div class="flex-col">
+                            <span class="forget-pwd-title">重置密码</span>
+                            <el-input class="ts-input" v-model="user_pwd_set" placeholder="请输入新密码，至少6位，不包含特殊字符"></el-input>
+                            <el-input class="ts-input" v-model="user_pwd_set2" placeholder="请再次输入密码"></el-input>
+                        </div>
+                        <div>
+                            <el-button class="ts-input" @click="confirm()" type="primary">提交</el-button>
+                        </div>
                     </div>
                 </div>
             </div>
